9ec9de36f964539d27c323456d4652152226dd2b
William Theesfeld <william@theesfeld.net>
2026-08-02 12:13:22 -0400

    chore: pin joule 0.1.4 from release SHA256SUMS

--- Formula/joule.rb
+++ Formula/joule.rb
@@ -1,28 +1,28 @@
 class Joule < Formula
   desc "Donate idle compute, earn millijoules, use open-weight AI"
   homepage "https://joule.f00.sh/"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-aarch64.tar.gz"
-      sha256 "5c77126ac13e137ebcfddb9194fec6d5d31d8d6d8ba5cb7f2d768a9f2a8adf92"
+      sha256 "bce63b5540d71ca7b1d4ad0436b7999077de2f4d1629a2d298ecdf26c7471d0a"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-x86_64.tar.gz"
-      sha256 "05f54dbdcad9c0e685155185ba9e036563b8798ddd59224071f2bbc2bd68370c"
+      sha256 "ab6ab0c95a228d5840bf110ff30d5dd71fef349c280b25b347dd92f656197212"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-aarch64.tar.gz"
-      sha256 "337af68f79a5eb70b3c9e48f57fc45f75a09dd40b3c57d1116bff215ff7062e8"
+      sha256 "ea21451547027732c107c9bfcd8303de1ba33b95cd942c4cc5700708206440ea"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-x86_64.tar.gz"
-      sha256 "5f77122c25f0eb83662329033e100a3919bc14af059657cc877a08ea1542470b"
+      sha256 "ea5a485bd546a9f7bfec1fac3ac5a10ac15c0cf610913a88550cced3ac6c630b"
     end
   end
 
@@ -33,7 +33,6 @@ class Joule < Formula
   end
 
   test do
-    # Binary Cargo version may lag SemVer tag on older cuts; 0.1.3 aligns package version.
     out = shell_output("#{bin}/joule version")
     assert_match(/joule\s+\d+\.\d+\.\d+/, out)
     assert_match(/protocol/, out)

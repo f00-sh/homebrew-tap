33b666014b7fa238d7f7b9095279bc73f51decec
William Theesfeld <william@theesfeld.net>
2026-08-02 21:29:57 -0400

    chore: pin joule 0.1.9

--- Formula/joule.rb
+++ Formula/joule.rb
@@ -1,26 +1,26 @@
 class Joule < Formula
   desc "Donate idle compute, earn millijoules, use open-weight AI"
   homepage "https://joule.f00.sh/"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
   on_macos do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-aarch64.tar.gz"
-      sha256 "abaebcc817b0b713d278e019b0bb384caa1d90694b2e57b180390d021e18cf0a"
+      sha256 "75bcb6090bf1b9af14b8f829de88fec25e75c5c43ea12a94b8b6cc9f8cf33d0f"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-darwin-x86_64.tar.gz"
-      sha256 "8254f0f5e1565ad0b3437faf02b101a05b1c1d57c542ea27d2ae450b2513fb0e"
+      sha256 "c482af60dea4d9aa613aee99146114b89630a82274e9387471a07b84f653c972"
     end
   end
   on_linux do
     on_arm do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-aarch64.tar.gz"
-      sha256 "f4e62d238d01b29569241f233942a48e2810ebb57e6a876a852eab741551f8a8"
+      sha256 "0d64f90d8e2307ff07e1cd001f020697a070504a474aa7b01f37f2b0a5164cd9"
     end
     on_intel do
       url "https://github.com/f00-sh/joule/releases/download/v#{version}/joule-#{version}-linux-x86_64.tar.gz"
-      sha256 "deca74b02998fb23963dcc02b48ab176a4eeb7f0f2e77ad6f1abf85a22024ed9"
+      sha256 "856baa9e720dbe5728f10aa4fdb41e6aeccb088c2efd8aeb8180777bb91a011c"
     end
   end
   def install
